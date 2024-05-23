d0b3b20688db5a55c40ee7563a11cbc751db9724
leonid <chemestru@gmail.com>
2024-05-23 14:01:43 +0300

    Small edits

--- Service/main.go
+++ Service/main.go
@@ -7,11 +7,27 @@ import (
 )
 
 func main() {
-	opt, err := redis.ParseURL("redis://<user>:<pass>@localhost:6379/<db>")
+	opt, err := redis.ParseURL("redis://leonid:password@redis:6379/users")
 	if err != nil {
 		panic(err)
 	}
 	
 	client := redis.NewClient(opt)
+
+	// Записать простую строку
+	ctx := context.Background()
+	key := "MyKey"
+	value := "MyValue"
+	err = client.Set(ctx, key, value, 0).Err()
+	if err != nil {
+		panic(err)
+	}
 	
+	// Извлечь строку
+	res, err := client.Get(ctx, key).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Key: %v, Value: %v\n", key, res)
 }
